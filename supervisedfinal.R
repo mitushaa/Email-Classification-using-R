6b899dea13050120371ec9294ab00537b2fd834f
Mitusha Arya <aryamitusha@gmail.com>
2019-11-04 20:55:03 +0530

    Update supervisedfinal.R

--- supervisedfinal.R
+++ supervisedfinal.R
@@ -8,7 +8,7 @@ library(plyr)
 library(ggplot2)
 library(RTextTools)
 library(e1071)
-
+//REad the table
 x <- read.table('5.txt', header=FALSE, sep='\t')
 unique(x$V1)
 x <- x[which(x$V1 %in% c('feedback','service', 'complaint')),]
